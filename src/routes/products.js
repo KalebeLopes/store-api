1edf7a9ac21f20e7ae5b5144b6d1e3c1b240793f
KalebeLopes <calbkalebe@gmail.com>
2021-03-05 14:49:45 -0300

    feat: route integration with the controller

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,12 @@
 import express from 'express'
+import ProductsController from '../controller/products'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send([{
-    name: 'Default product',
-    description: 'product description',
-    price: 100
-  }])
+const productsController = new ProductsController()
+
+router.get('/', (req, res) => {  
+  productsController.get(req, res);
 })
 
 export default router
