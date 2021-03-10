1cf4568f781df29d2d062b6d9dae22fb23452e4e
KalebeLopes <calbkalebe@gmail.com>
2021-03-10 12:20:51 -0300

    feat: route get for return on product

--- src/controller/products.js
+++ src/controller/products.js
@@ -12,6 +12,21 @@ class ProductsController {
     }
   }
 
+  async getById (req, res) {
+    const {
+      params: {id}
+    } = req
+
+    try {
+      const product = await this.Product.findOne({_id: id})
+      // console.log(product)
+      res.send(product)
+    } catch (err) {
+      res.status(400).send(err.message)
+      //console.error('400')
+    }
+  }
+
 }
 
 export default ProductsController
