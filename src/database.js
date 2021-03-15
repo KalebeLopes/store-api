73626f73ade18504a513836afcdbdfcc81ba770d
KalebeLopes <calbkalebe@gmail.com>
2021-03-15 11:58:45 -0300

    refactor: refactor architecture

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
+import config from 'config'
 
-const mondodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/test'
+const mondodbUrl = config.get('database.mongoUrl')
 
 const connect = () => mongoose.connect(mondodbUrl, {
   useNewUrlParser: true,
