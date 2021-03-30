13bf08194b0d68c446f6ca323cc5cee06b206382
KalebeLopes <calbkalebe@gmail.com>
2021-03-30 16:52:32 -0300

    refactor: update database config

--- src/database.js
+++ src/database.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose'
-import config from 'config'
+import mongoose from "mongoose";
+import config from "config";
 
-const mondodbUrl = config.get('database.mongoUrl')
+const mondodbUrl = config.get("database.mongoUrl");
 
-const connect = () => mongoose.connect(mondodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+const connect = () =>
+  mongoose.connect(mondodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 export default {
   connect,
   connection: mongoose.connection
-}
+};
