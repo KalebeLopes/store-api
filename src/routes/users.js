2a058a596ffed1f3a0e05a292f3ed02f57bf8e18
KalebeLopes <calbkalebe@gmail.com>
2021-03-30 16:59:12 -0300

    feat: create authenticate endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,32 +1,37 @@
-import express from 'express'
-import UsersController from '../../src/controller/users'
-import User from '../../src/models/user'
+import express from "express";
+import UsersController from "../../src/controller/users";
+import User from "../../src/models/user";
+import AuthUser from "../services/auth";
 
-const router = express.Router()
-const usersController = new UsersController(User)
+const router = express.Router();
+const usersController = new UsersController(User, AuthUser);
 
-router.get('/', (req, res) => {
-  usersController.get(req, res)
-})
+router.get("/", (req, res) => {
+  usersController.get(req, res);
+});
 
-router.get('/:id', (req, res) => {
-  usersController.getById(req, res)
-})
+router.get("/:id", (req, res) => {
+  usersController.getById(req, res);
+});
 
-router.post('/', (req, res) => {
-  usersController.createUser(req, res)
-})
+router.post("/", (req, res) => {
+  usersController.createUser(req, res);
+});
 
-router.put('/:id', (req, res) => {
-  usersController.updateUser(req, res)
-})
+router.put("/:id", (req, res) => {
+  usersController.updateUser(req, res);
+});
 
-router.delete('/', (req, res) => {
-  usersController.deleteAll(req, res)
-})
+router.delete("/", (req, res) => {
+  usersController.deleteAll(req, res);
+});
 
-router.delete('/:id', (req, res) => {
-  usersController.deleteById(req, res)
-})
+router.delete("/:id", (req, res) => {
+  usersController.deleteById(req, res);
+});
 
-export default router
+router.post("/authenticate", (req, res) => {
+  usersController.authenticateUser(req, res);
+});
+
+export default router;
