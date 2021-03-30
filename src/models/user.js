5b35133bbf7aed5c2a133c736fd593d16c6a5fac
KalebeLopes <calbkalebe@gmail.com>
2021-03-30 16:57:00 -0300

    refactor: update with eslint and prettier

--- src/models/user.js
+++ src/models/user.js
@@ -1,36 +1,38 @@
-import mongoose from 'mongoose'
-import Util from 'util'
-import bcrypy from 'bcrypt'
+import mongoose from "mongoose";
+import Util from "util";
+import bcrypy from "bcrypt";
 
-const hashAsync = Util.promisify(bcrypy.hash)
+const hashAsync = Util.promisify(bcrypy.hash);
 const schema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   role: String
-})
+});
 
-schema.pre('save', async function(next) {
-  if (!this.password || !this.isModified('password')) {
-    return next()
+schema.pre("save", async function(next) {
+  if (!this.password || !this.isModified("password")) {
+    return next();
   }
   try {
-    const hashedPassword = await hashAsync(this.password, '10')
-    this.password = hashedPassword
-  } catch(err) {
-    next(err)
+    const hashedPassword = await hashAsync(this.password, 10);
+    this.password = hashedPassword;
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-schema.set('toJSON', {  // utilizado para intervir na serialização e customizar o resultado final do obj 
-  transform: (doc, ret, options) => ({ // transform para remover o password do obj final 
+schema.set("toJSON", {
+  // utilizado para intervir na serialização e customizar o resultado final do obj
+  transform: (doc, ret, options) => ({
+    // transform para remover o password do obj final
     _id: ret._id,
     email: ret.email,
     name: ret.name,
     role: ret.role
   })
-})
+});
 
-const User = mongoose.model('User', schema)
- 
-export default User
+const User = mongoose.model("User", schema);
+
+export default User;
